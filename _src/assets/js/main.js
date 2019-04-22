8313c47ee158587459af327a52df27abe51274f3
irenegf83 <irene@irenegf.com>
2019-04-22 15:27:12 +0200

    Initial script

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+// HTML
+    // campo de búsqueda
+    // botón Buscar
+    // ul para almacenar series favoritas
+    // ul para almacenar listado de series
+
+
+// JS
+// Recoger
+    // input busqueda
+    // botón
+    // ul series favoritas
+    // ul series
+    
+// al hacer click en el botón de buscar 
+    // nos conectaremos a una api de series dónde se buscará lo que el usuario introduzca en el campo de búsqueda
+    // pintar una tarjeta con:
+        // título serie
+        // imagen serie o imagen por defecto (https://via.placeholder.com/210x295/ffffff/666666/?text=TV)
+
+    // al hacer click en una serie se marca como favorita
+        // las marcadas se guradarán en un array
+        // se mostrarán en el lado izquierdo de la pantalla (debajo del formulario)
+        // al hacer nueva búsqueda los favoritos se van acumulando
+        // se guardan el LocalStorage y al recargar página siempre se verán
